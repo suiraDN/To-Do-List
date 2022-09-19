1874544b14e4e4dcb69a18edde3ce75cc776c55d
Darius Navickas <dariusnaw@gmail.com>
2022-09-19 21:46:17 +0300

    empty input box fix

--- app.js
+++ app.js
@@ -17,26 +17,30 @@ function addTodo(event) {
 	const todoDiv = document.createElement("div");
 	todoDiv.classList.add("todo");
 	//Create LI
-	const newTodo = document.createElement("li");
-	newTodo.innerText = todoInput.value;
-	newTodo.classList.add("todo-item");
-	todoDiv.appendChild(newTodo);
-	//Add todo to localstorage
-	saveLocalTodos(todoInput.value);
-	//Check mark button
-	const completedButton = document.createElement("button");
-	completedButton.innerHTML = '<i class="fas fa-check"></i>';
-	completedButton.classList.add("complete-btn");
-	todoDiv.appendChild(completedButton);
-	//Check trash button
-	const trashButton = document.createElement("button");
-	trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-	trashButton.classList.add("trash-btn");
-	todoDiv.appendChild(trashButton);
-	//Append to list
-	todoList.appendChild(todoDiv);
-	//Clear Todo INPUT VALUE
-	todoInput.value = "";
+	if (todoInput.value === "") {
+		alert("Enter your text");
+	} else {
+		const newTodo = document.createElement("li");
+		newTodo.innerText = todoInput.value;
+		newTodo.classList.add("todo-item");
+		todoDiv.appendChild(newTodo);
+		//Add todo to localstorage
+		saveLocalTodos(todoInput.value);
+		//Check mark button
+		const completedButton = document.createElement("button");
+		completedButton.innerHTML = '<i class="fas fa-check"></i>';
+		completedButton.classList.add("complete-btn");
+		todoDiv.appendChild(completedButton);
+		//Check trash button
+		const trashButton = document.createElement("button");
+		trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+		trashButton.classList.add("trash-btn");
+		todoDiv.appendChild(trashButton);
+		//Append to list
+		todoList.appendChild(todoDiv);
+		//Clear Todo INPUT VALUE
+		todoInput.value = "";
+	}
 }
 
 function deleteCheck(e) {
